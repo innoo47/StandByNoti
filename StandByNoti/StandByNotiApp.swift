f4f98496ca9a50bdf837d26f92606f917615453a
Inu <i0n4h0o7@gmail.com>
2024-03-16 15:26:17 +0900

    Initial Commit

---
+++ StandByNoti/StandByNotiApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandByNotiApp.swift
+//  StandByNoti
+//
+//  Created by 박인호 on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct StandByNotiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
